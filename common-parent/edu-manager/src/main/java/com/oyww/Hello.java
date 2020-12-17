ce968af5cb0dd9d9537201f665508bc9bea6c197
oyww <289002360@qq.com>
2020-12-17 16:19:48 +0800

    建立新文件

--- common-parent/edu-manager/src/main/java/com/oyww/Hello.java
+++ common-parent/edu-manager/src/main/java/com/oyww/Hello.java
@@ -3,5 +3,8 @@ package com.oyww;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
