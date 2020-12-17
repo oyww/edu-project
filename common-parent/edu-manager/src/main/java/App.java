a4caf12c1c5f37660b935bdb4d9b69e2d6aeab82
oyww <289002360@qq.com>
2020-12-17 15:49:23 +0800

    删除打印内容

--- common-parent/edu-manager/src/main/java/App.java
+++ common-parent/edu-manager/src/main/java/App.java
@@ -1,7 +1,4 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
     }
 }
